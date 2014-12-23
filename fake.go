57229fb221b090f8d79097a17cf3d440376f7513
Dmitry Afanasyev <dimarzio1986@gmail.com>
2014-12-23 18:54:26 +0300

    fix docs

--- fake.go
+++ fake.go
@@ -6,28 +6,28 @@ Currently english and russian languages are available.
 Feel free to add other languages, but don't forget to regenerate data.go file using `github.com/mjibson/esc` tool and `esc -o data.go -pkg fake data` command because Fake embeds files unless you call `UseExternalData(true)` in order to be able to work file without dependencies when compiled.
 
 Examples
- name := fake.FirstName()
- fullname = := fake.FullName()
- product := fake.Product()
+	name := fake.FirstName()
+	fullname = := fake.FullName()
+	product := fake.Product()
 
 Changing language:
- err := fake.SetLang("ru")
- if err != nil {
-   panic(err)
- }
- password := fake.SimplePassword()
+	err := fake.SetLang("ru")
+	if err != nil {
+		panic(err)
+	}
+	password := fake.SimplePassword()
 
 Using english fallback:
- err := fake.SetLang("ru")
- if err != nil {
-   panic(err)
- }
- fake.EnFallback(true)
- password := fake.Paragraph()
+	err := fake.SetLang("ru")
+	if err != nil {
+		panic(err)
+	}
+	fake.EnFallback(true)
+	password := fake.Paragraph()
 
 Using external data:
-  fake.UseExternalData(true)
-  password := fake.Paragraph()
+	fake.UseExternalData(true)
+	password := fake.Paragraph()
 */
 package fake
 
