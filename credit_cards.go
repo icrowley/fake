ae333bb818f1c9943ae3169b66684334a4634138
Dave Eargle <dave@daveeargle.com>
2022-06-22 16:05:48 -0600

    fix credit card number generation
    closes #32

--- credit_cards.go
+++ credit_cards.go
@@ -45,25 +45,44 @@ func CreditCardNum(vendor string) string {
 	card := creditCards[vendor]
 	prefix := strconv.Itoa(card.prefixes[r.Intn(len(card.prefixes))])
 	num := []rune(prefix)
-	for i := 0; i < card.length-len(prefix); i++ {
-		num = append(num, genCCDigit(num))
+
+	// -1 to leave room for the checksum
+	for i := 0; i < card.length-len(prefix)-1; i++ {
+		num = append(num, random_int_rune())
 	}
+
+	// and now add the checksum
+	num = append(num, genCCDigit(num))
 	return string(num)
 }
 
+func random_int_rune() rune {
+	// random int [0,9) => string => array of runes => single rune
+	return []rune(strconv.Itoa(r.Intn(10)))[0]
+}
+
 func genCCDigit(num []rune) rune {
+	// See https://en.wikipedia.org/wiki/Luhn_algorithm
+	// But be sure to also read the sordid "talk" page --
+	// the example implementations have errors.
 	sum := 0
+	pos := 0
 	for i := len(num) - 1; i >= 0; i-- {
-		n := int(num[i])
-		if i%2 != 0 {
-			sum += n
-		} else {
-			if n*2 > 9 {
-				sum += n*2 - 9
-			} else {
-				sum += n * 2
+		// The next line is equivalent to `n, _ := strconv.Atoi(string(num[i]))`
+		n := int(num[i] - '0')
+		if pos % 2 == 0 {
+			n *= 2
+			if n > 9 {
+				n -= 9
 			}
 		}
+		sum += n
+		pos++
 	}
-	return rune(((sum/10+1)*10 - sum) % 10)
+
+	// https://en.wikipedia.org/wiki/Talk:Luhn_algorithm#Formula_error
+	checksum := (10 - (sum % 10) % 10)
+
+	// int => string => array of runes => single rune
+	return []rune(strconv.Itoa(checksum))[0]
 }
